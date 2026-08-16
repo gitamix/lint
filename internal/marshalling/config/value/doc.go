fd08eaf204da8a50b9f2bb8ce46a3a34050fae9c
Ilya Sitnikov <sitnikovik@gmail.com>
2026-08-16 11:54:57 +0300

    docs(config): marshalling values package

---
+++ internal/marshalling/config/value/doc.go
@@ -0,0 +1,6 @@
+// Package value provides the transport structures
+// used to unmarshal the config value types like string patterns,
+// integer ranges, etc. from config files
+// and convert them into the matching domain values
+// through their Config methods.
+package value
