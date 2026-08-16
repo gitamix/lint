e9e0cbe188de5413e7ce7e00402e8c6045436736
Ilya Sitnikov <sitnikovik@gmail.com>
2026-08-16 18:15:26 +0300

    feat(config): marshalling commit types

---
+++ internal/marshalling/config/commit/types/doc.go
@@ -0,0 +1,3 @@
+// Package types provides the transport structures used to unmarshal the
+// accepted commit types configuration from config files.
+package types
---
+++ internal/marshalling/config/commit/types/types.go
@@ -0,0 +1,20 @@
+package types
+
+import (
+	"github.com/gitamix/lint/config/commit/types"
+	"github.com/gitamix/lint/internal/marshalling/config/value"
+)
+
+// Types is the transport representation of the accepted commit types config.
+type Types struct {
+	// Types stores the transport representation of the accepted commit types.
+	Types value.Strings `yaml:"types,omitempty"`
+}
+
+// Config converts the transport representation into the domain types.Config,
+// wiring the accepted commit types into it.
+func (c Types) Config() types.Config {
+	return types.NewConfig(
+		c.Types.Config(),
+	)
+}
